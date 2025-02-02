6d9033009b9d84ff5df531788d83e3cfb884bc36
Connor Watson <cjw140497@gmail.com>
2025-02-02 16:40:51 +0000

    Updated About Me And Added Mobile Compatability i have adde3d mobile compatability by using @media in css and using a java script to know when the browser is is a certain size to activate a drop down burger style menu

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 
 console.log("Script is running!");
 
+// Select the menu toggle button and navigation links
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+// Toggle the visibility of nav links when the hamburger button is clicked
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
